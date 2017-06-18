6a1e67a27fab28bf1b6ccb5f7dd3aed27640c329
cfaeth <cfaeth@c2c37a5c-0e1b-4e2a-869e-6f6e14130990>
2017-06-18 15:45:49 +0000

    analyze-ud-tags.sh: Removed some overhead, adjusted paths. Works as before.
    git-svn-id: https://valian.uni-frankfurt.de/svn/repository/intern/incubator/conll-rdf/_release@6773 c2c37a5c-0e1b-4e2a-869e-6f6e14130990

--- analyze-ud-tags.sh
+++ analyze-ud-tags.sh
@@ -5,35 +5,16 @@
 # we test to what extent syntactic criteria are overridden by other criteria
 # approach: map UD tags to syntactic prototypes, derive syntactic prototypes from UD deps, compare
 
-FILES=$*;
-HOME=`echo $0 | sed s/'\/[^\/]*$'/'\/'/`./;
-
-if echo $FILES | grep '^$'; then
-	FILES=`find $HOME | egrep 'conllu(.gz)?$'`;
-fi;
-
-if echo $FILES | grep '^$'; then
-	echo 'read from stdin' 1>&2;
-	cat;							# no conll files found, read from stdin
-else 
-	for file in $FILES; do
-		if echo $file | grep 'gz$' >&/dev/null; then
-			gunzip -c $file; 
-		else 	
-			cat $file; 
-		fi;
-	done;
-fi	| \
+for file in `find ud | grep 'dev.conllu.gz$'`; do gunzip -c $file; done | \
 egrep '^[0-9]+\s.*$|^$' |								# remove multiword tokens
-(cd ..; 
 ./run.sh \
 	CoNLLStreamExtractor https://github.com/UniversalDependencies/UD_English# \
 		IGNORE WORD IGNORE UPOS IGNORE IGNORE HEAD EDGE IGNORE IGNORE \
 		-u sparql/remove-IGNORE.sparql \
-		   ud/UPOS-to-POSsynt.sparql \
-		   ud/EDGE-to-POSsynt.sparql \
-		   ud/consolidate-POSsynt.sparql \
-		-s ud/eval-POSsynt.sparql \
+		   analyze-ud-tags/UPOS-to-POSsynt.sparql \
+		   analyze-ud-tags/EDGE-to-POSsynt.sparql \
+		   analyze-ud-tags/consolidate-POSsynt.sparql \
+		-s analyze-ud-tags/eval-POSsynt.sparql \
 	| grep -v '#'
 		# | \
 # # cat; echo |\
