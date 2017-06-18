b41903ff18ce1fa41566e527933083eaf149517b
cfaeth <cfaeth@c2c37a5c-0e1b-4e2a-869e-6f6e14130990>
2017-06-18 15:18:24 +0000

    olia-linking now works fine for UPOS. However, I used the experimental AM/LM from univ_dep/all_from_rdfa since the other UD models are inconsistent with their use of namespaces, classes and individuals (or at least I did not find a working combination).
    git-svn-id: https://valian.uni-frankfurt.de/svn/repository/intern/incubator/conll-rdf/_release@6772 c2c37a5c-0e1b-4e2a-869e-6f6e14130990

--- olia-linking.sh
+++ olia-linking.sh
@@ -2,12 +2,17 @@
 # example pipeline for processing German Universal Dependencies
 
 # 1. read
-for file in `find ud/1.4 | grep 'test.conllu$'`; do echo $file; tmp=UD_${file#*UD_}; lang=${tmp%/*}; cat $file | \
+for file in `find ud | grep 'conllu.gz$'`; do echo $file; filename=$(basename "$file"); tmp=UD_${file#*UD_}; lang=${tmp%-master/*}; gunzip -c $file | \
 # 2. parse UD data to RDF
 ./run.sh \
 	CoNLLStreamExtractor https://github.com/UniversalDependencies/$lang# \
 	ID WORD LEMMA UPOS POS FEAT HEAD EDGE DEPS MISC | \
-\
+./run.sh CoNLLRDFUpdater -custom \
+	-model http://purl.org/olia/owl/experimental/univ_dep/all_from_rdfa/ud-pos-all.owl http://purl.org/olia/ud-pos-all.owl \
+	-model http://purl.org/olia/owl/experimental/univ_dep/all_from_rdfa/ud-pos-all-link.rdf http://purl.org/olia/ud-pos-all.owl \
+	-updates olia-linking/link-UPOS-simple.sparql{1} \
+	   \
+| \
 # 3. format
-./run.sh CoNLLRDFFormatter -rdf ID WORD LEMMA UPOS POS FEAT HEAD EDGE DEPS MISC $* > ud/ttl/$lang.ttl \
+./run.sh CoNLLRDFFormatter -rdf ID WORD LEMMA UPOS POS FEAT HEAD EDGE DEPS MISC $* > ud/ttl/$filename.linked.ttl \
 ; done
