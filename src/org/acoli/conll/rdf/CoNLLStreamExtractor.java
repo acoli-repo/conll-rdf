6dd841dbbb2b4ba592db7f3fc14e9ed547b5abf1
deshoe20 <g.benjamin@gmx.de>
2017-11-01 14:15:33 +0100

    * added {*} functionality as an optional parameter to iterate over an update script as long as there are still changes to the sentence model (max of 999 iterations after which an error msg is printed on std.err) * added debug information - the cumulative runtime for each update in order of the sparql updates scripts given

--- src/org/acoli/conll/rdf/CoNLLStreamExtractor.java
+++ src/org/acoli/conll/rdf/CoNLLStreamExtractor.java
@@ -4,9 +4,11 @@ import java.io.*;
 import java.net.*;
 import java.util.*; 
 // import org.apache.jena.rdf.model.*;	// Jena 3.x
+import com.hp.hpl.jena.rdf.listeners.ChangedListener;
 import com.hp.hpl.jena.rdf.model.*;		// Jena 2.x
 import com.hp.hpl.jena.update.*;
 import com.hp.hpl.jena.query.*;
+import javafx.util.Pair;
 
 /** extracts RDF data from CoNLL files, transforms the result using SPARQL UPDATE queries,
  * 	optionally followed by SPARQL SELECT to produce TSV output<br>
@@ -14,6 +16,8 @@ import com.hp.hpl.jena.query.*;
  *  a literal query string may be parsed by the JVM/shell
  */
 public class CoNLLStreamExtractor {
+
+	static final int MAXITERATE = 999; // maximal update iterations allowed until the update loop is canceled and an error msg is thrown - to prevent faulty update scripts running in an endless loop
 	
 	public static void main(String[] argv) throws Exception {
 		System.err.println("synopsis: CoNLLStreamExtractor baseURI FIELD1[.. FIELDn] [-u SPARQL_UPDATE1..m] [-s SPARQL_SELECT]\n"+
@@ -21,6 +25,7 @@ public class CoNLLStreamExtractor {
 			"\tFIELDi        CoNLL field label, cf. CoNLL2RDF\n"+
 			"\tSPARQL_UPDATE SPARQL UPDATE (DELETE/INSERT) query, either literally or its location (file/uri)\n"+
 			"\t              can be followed by an optional integer in {}-parentheses = number of repetitions\n"+
+			"\t              or optional asterisk {*} to repeat until there are no more changes in the model\n"+
 			"\tSPARQL_SELECT SPARQL SELECT statement to produce TSV output\n"+
 			"reads CoNLL from stdin, "
 			+ "splits sentences, "
@@ -29,7 +34,7 @@ public class CoNLLStreamExtractor {
 		
 		String baseURI = argv[0];
 		List<String> fields = new ArrayList<String>();
-		List<String> updates = new ArrayList<String>();
+		List<Pair<String, String>> updates = new ArrayList<Pair<String, String>>();
 		String select = null;
 		
 		int i = 1;
@@ -37,15 +42,12 @@ public class CoNLLStreamExtractor {
 			fields.add(argv[i++]);
 		while(i<argv.length && argv[i].toLowerCase().matches("^-+u$")) i++;
 		while(i<argv.length && !argv[i].toLowerCase().matches("^-+s$")) {
-			int freq = 1;
-			try {
-				freq = Integer.parseInt(argv[i].replaceFirst(".*\\{([0-9]+)\\}$", "$1"));
-			} catch (NumberFormatException e) {}
-			String update =argv[i++].replaceFirst("\\{[0-9]+\\}$", "");
-			while(freq>0) {
-				updates.add(update);
-				freq--;
-			}
+			String freq;
+			freq = argv[i].replaceFirst(".*\\{([0-9*]+)\\}$", "$1");
+			if (argv[i].equals(freq))
+				freq = "1";
+			String update =argv[i++].replaceFirst("\\{[0-9*]+\\}$", "");
+			updates.add(new Pair(update, freq));
 		}
 		while(i<argv.length && argv[i].toLowerCase().matches("^-+s$")) i++;
 		if(i<argv.length)
@@ -62,13 +64,13 @@ public class CoNLLStreamExtractor {
 		System.err.print("read SPARQL ..");
 		//UpdateRequest request = UpdateFactory.create();
 		for(i = 0; i<updates.size(); i++) {
-			Reader sparqlreader = new StringReader(updates.get(i));
-			File f = new File(updates.get(i));
+			Reader sparqlreader = new StringReader(updates.get(i).getKey());
+			File f = new File(updates.get(i).getKey());
 			URL u = null;
 			try {
-				u = new URL(updates.get(i));
+				u = new URL(updates.get(i).getKey());
 			} catch (MalformedURLException e) {}
-			
+
 			if(f.exists()) {			// can be read from a file
 				sparqlreader = new FileReader(f);
 				System.err.print("f");
@@ -79,10 +81,10 @@ public class CoNLLStreamExtractor {
 				} catch (Exception e) {}
 			}
 
-			updates.set(i,"");
+			updates.set(i,new Pair("", updates.get(i).getValue()));
 			BufferedReader in = new BufferedReader(sparqlreader);
 			for(String line = in.readLine(); line!=null; line=in.readLine())
-				updates.set(i,updates.get(i)+line+"\n");
+				updates.set(i,new Pair(updates.get(i).getKey()+line+"\n",updates.get(i).getValue()));
 			System.err.print(".");
 		}
 		System.err.print(".");
@@ -113,8 +115,8 @@ public class CoNLLStreamExtractor {
 		System.err.println(". ok");
 		
 		CoNLL2RDF conll2rdf = new CoNLL2RDF(baseURI, fields.toArray(new String[fields.size()]));
-		
-		System.err.print("process input ..");
+		List<Long> dRTs = new ArrayList<Long>(); // TODO : move to debug // should be execution time of each update in seconds
+		System.err.println("process input ..");
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		OutputStreamWriter out = new OutputStreamWriter(System.out);
 		String buffer = "";
@@ -126,7 +128,12 @@ public class CoNLLStreamExtractor {
 				if(line.equals("") && !buffer.trim().equals("")) {
 					Model m = conll2rdf.conll2model(new StringReader(buffer+"\n"));
 					if(m!=null) { // null if an error occurred
-						update(m, updates); 
+						List<Long> ret = update(m, updates);
+						if (dRTs.isEmpty())
+							dRTs = ret;
+						else
+							for (int x = 0; x < ret.size(); ++x)
+								dRTs.set(x, dRTs.get(x) + ret.get(x));
 						print(m,select, out);
 					}
 					buffer="";
@@ -135,14 +142,45 @@ public class CoNLLStreamExtractor {
 		}
 		if(!buffer.trim().equals("")) {
 			Model m = conll2rdf.conll2model(new StringReader(buffer+"\n"));
-			update(m,updates); 
+			List<Long> ret = update(m, updates);
+			if (dRTs.isEmpty())
+				dRTs = ret;
+			else
+				for (int x = 0; x < ret.size(); ++x)
+					dRTs.set(x, dRTs.get(x) + ret.get(x));
 			print(m,select,out);
 		}
+		System.err.println("List of execution times for the updates done: " + dRTs.toString());
 	}
 	
-	public static void update(Model m, List<String> updates) {
-		for(String update : updates)
-			UpdateAction.execute(UpdateFactory.create(update), m);
+	public static List<Long> update(Model m, List<Pair<String,String>> updates) {
+		List<Long> result = new ArrayList<Long>();
+		for(Pair<String,String> update : updates) {
+			Long startTime = System.currentTimeMillis();
+			ChangedListener cL = new ChangedListener();
+			m.register(cL);
+			int frq = 0;
+			boolean change = false;
+			try {
+				frq = Integer.parseInt(update.getValue());
+			} catch (NumberFormatException e) {
+				if ("*".equals(update.getValue()))
+					change = true;
+				else
+					throw e;
+			}
+			while(frq > 0 || change && ((frq * -1) < MAXITERATE)) {
+				UpdateAction.execute(UpdateFactory.create(update.getKey()), m);
+				if (change)
+					change = cL.hasChanged();
+				if (frq > 0)
+					frq--;
+			}
+			if ((frq * -1) == MAXITERATE)
+				System.err.println("Warning: MAXITERATE reached.");
+			result.add(System.currentTimeMillis() - startTime);
+		}
+		return result;
 	}
 		
 	/** run either SELECT statement (cf. https://jena.apache.org/documentation/query/app_api.html) and return CoNLL-like TSV or just TTL <br>
