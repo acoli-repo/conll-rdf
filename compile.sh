3967888831fa7da633c344add6094db5cb028e9b
leogott <61663141+leogott@users.noreply.github.com>
2020-09-14 21:20:52 +0200

    adapt run.sh compile.sh

--- compile.sh
+++ compile.sh
@@ -1,45 +1,35 @@
 #!/bin/bash
-# determines the classpath, updates class files if necessary and runs the specified java class with the provided arguments
-# must be run from $HOME: do not call directly, indirectly evoked via ./run.sh
+# compile all missing and outdated java classes, and copy .properties files over.
+# requires $HOME, $TARGET, and $CLASSPATH to be set, or maven to be installed.
+# do not call directly, indirectly invoked via ./run.sh
 
-HOME=`echo $0 | sed -e s/'[^\/]*$'//`'.';
-cd $HOME
-HOME=`pwd -P`;
-cd - >&/dev/null;
+mkdir -p "$TARGET";
 
-mkdir $HOME/bin >& /dev/null;
-
-TGT=$HOME/bin;
-
-CLASSPATH=$HOME/bin":"`find $HOME/lib | perl -pe 's/\n/:/g;' | sed s/':$'//`;
-if [ $OSTYPE = "cygwin" ]; then
-	TGT=`cygpath -wa $HOME/bin`;
-	CLASSPATH=$TGT;
-	for lib in `find $HOME/lib`; do
-		CLASSPATH=$CLASSPATH';'`cygpath -wa $lib`
-	done;
-fi;
-
-JAVAS=$(
-	for java in `find . | sed s/'^\.\/'// | egrep 'java$'`; do
-		class=`echo $java | sed s/'java$'/'class'/;`
-		if [ ! -e $class ]; then
-			echo $java;
-		else if [ $java -nt $class ]; then
-			echo $java;
-			fi;
-		fi;
-	done;
-	)
-for propertiesFile in `find ./src | sed s/'^\.\/'// | egrep 'properties$'`; do
-	cp -f $propertiesFile ${propertiesFile/'src'/'bin'} >& /dev/null;
+JAVAS="";
+for java in $(find $HOME/src/main/java -name "*.java"); do
+	class=$TARGET${java#$HOME/src/main/java}
+	class=${class%.java}.class
+	if [ ! -e $class ] || [ $java -nt $class ]; then
+		JAVAS="$JAVAS $java"
+	fi;
 done;
-if
-	if echo $JAVAS | grep java >/dev/null; then
-		javac -d $TGT -classpath $CLASSPATH $JAVAS;
-	fi 2>&1;
-then
-	echo >& /dev/null;
-else
-	javac -d $TGT -g -classpath $CLASSPATH $*; 
+
+for propertiesFile in $(find $HOME/src/main/resources -name '*.properties'); do
+	cp -f -- "$propertiesFile" "$TARGET${propertiesFile#src/main/resources}" &> /dev/null;
+done;
+
+if [ -n "$JAVAS" ]; then
+	if [ $OSTYPE = "cygwin" ]; then
+		TARGET=$(cygpath -wa -- "$TARGET");
+		CLASSPATH=$(cygpath -pwa -- "$CLASSPATH");
+		JAVAS=$(for java in $JAVAS; do cygpath -wa -- "$java"; done);
+	fi;
+	javac -d $TARGET -classpath $CLASSPATH $JAVAS;
 fi;
+
+# debug
+# echo 'ARGS:' $*
+# echo 'HOME:' $HOME
+# echo 'TARGET:' $TARGET
+# echo 'CLASSPATH:' $CLASSPATH
+# echo 'JAVAS:' $JAVAS;
