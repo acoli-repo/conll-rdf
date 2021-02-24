ad8dc394bd22cb7d428a68cb95daf6b7eb68d074
leogott <61663141+leogott@users.noreply.github.com>
2021-02-24 15:19:17 +0100

    Switch to Maven for Java Project Building (#37)
    Rebuild for maven compatibility. 
    Adjusted Folder structure according to Maven template.
    Checks for Maven. If it does not exist, falls back to original run.sh/compile.sh.
    Tested for use on native Linux and Cygwin environments.
    
    History: 
    
    * Apply Maven Archetype
    
    * Apply Maven Archetype
    
    * Add Maven Dependencies
    Used the exact versions found in lib/, but skipped all the unused ones.
    
    * adapt run.sh compile.sh
    
    * Update documentation wrt Maven
    
    * Fix run.sh by piping compile output to null
    
    * add .gitattributes to fix line endings + cygwin
    
    * Remove unused classes created by maven archetype
    
    * Pom.xml correct java version to 1.8
    and update junit version to junit-jupiter 5.7.0
    
    * Restructure shell shims to call maven less often
    
    * Fix: update Java-Version in .classpath

--- run.sh
+++ run.sh
@@ -1,49 +1,23 @@
 #!/bin/bash
-# determines the classpath, updates class files if necessary and runs the specified java class with the provided arguments
-# we also add org.acoli.conll.rdf as package, so, call the class by local name, only, see examples
+# run the specified java class (of the org.acoli.conll.rdf package) with the provided arguments
+# after calling compile.sh to ensure it's up to date.
 
-HOME=`echo $0 | sed -e s/'[^\/]*$'//`'.';
-cd $HOME
-HOME=`pwd -P`;
-cd - >&/dev/null;
+# store the path of the conll-rdf directory in HOME
+HOME=$( dirname -- "$(realpath -- "$0")");
+TARGET="$HOME/target/classes";
+CLASSPATH="$TARGET:$HOME/lib/*";
 
-TGT=$HOME/bin
-
-mkdir $TGT >&/dev/null;
-
-CLASSPATH=$TGT":"`find $HOME/lib | perl -pe 's/\n/:/g;' | sed s/':$'//`;
-if [ $OSTYPE = "cygwin" ]; then
-	TGT=`cygpath -wa $HOME/bin`;
-	CLASSPATH=$TGT;
-	for lib in `find $HOME/lib`; do
-		CLASSPATH=$CLASSPATH';'`cygpath -wa $lib`
-	done;
-fi;
-
-JAVAS=$(
-	cd $HOME;
-	for java in `find  . | egrep 'java$'`; do
-		class=`echo $java | sed -e s/'src\/'/'bin\/'/ -e s/'java$'/'class'/;`
-		if [ ! -e $class ]; then
-			echo $java;
-		else if [ $java -nt $class ]; then
-			echo $java;
-			fi;
-		fi;
-	done;
-	)
-
-if
-	if echo $JAVAS | grep java >/dev/null; then
-		cd $HOME;
-		./compile.sh;
-		cd - >&/dev/null;
-	fi 2>&1;
-then 
-	#could also add  -Dlog4j.configuration=file:'src/log4j.properties' for another log4j config
+if (source $HOME/compile.sh &> /dev/null;); then
+	if [ $OSTYPE = "cygwin" ]; then
+		TARGET=$(cygpath -wa -- "$TARGET");
+		CLASSPATH=$(cygpath -pwa -- "$CLASSPATH");
+	fi;
+	#could also add -Dlog4j.configuration=file:'src/main/resources/log4j.properties' for another log4j config
 	java -Dfile.encoding=UTF8 -classpath $CLASSPATH org.acoli.conll.rdf.$* | \
 	# the following is a hack to allow CoNLLRDFUpdater to process output of CoNLLBrackets2RDF
 	# currently, CoNLLRDFUpdater supportes the historical Turtle 1.0 prefix only
 	# TODO: fix this in CoNLLRDFUpdater, this is a hack, only
 	sed -e s/'^[\t ]*PREFIX \(.*\)$'/'@prefix \1 .'/g;
+else
+	echo "error: compile unsuccessful"
 fi;
