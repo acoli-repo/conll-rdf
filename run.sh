7188f802b681b01dfdd9c65019ed94e8906667b9
leogott <61663141+leogott@users.noreply.github.com>
2022-01-27 16:39:14 +0100

    Fix run.sh wrt. maven.args and spaces in paths

--- run.sh
+++ run.sh
@@ -23,6 +23,13 @@
 conll_dir="$(dirname -- "$(realpath -- "$0")")"
 # use like target_dir="${conll_dir}/target/classes"
 
+# join(char seperator, arg1 ... argn) concatenates an array with the seperating character
+function join {
+  local IFS="$1"
+  shift
+  printf '%s\n' "$*"
+}
+
 # if the number of arguments is equal to 0, no class name is provided
 if [ $# -eq 0 ]; then
 	echo "Please give the name of the conll-rdf class you want to run"
@@ -32,19 +39,21 @@ fi
 # first cli argument
 class_name="$1"
 # "consume" 1 argument
-shift 1;
+shift
 # $@ is a special variable containing the arguments passed to the shell script.
 # ${variable@Q} is "Parameter Transformation" to quote the content of the array for re-use
 # as the variable is $@ is this case, ${}
 # $@ is the variable of the arguments. For a wrapper to java you'd use ("$@")
 # For an explanation to the use of quotes, see the reference at the top of this file.
-quoted_args="${@@Q}"
+args_array=("${@@Q}")
+# join the quoted args into a single string
+args=$(join ' ' "${args_array[@]}")
 
-mvn_args=("--batch-mode" "--quiet" "--file=${conll_dir}")
+mvn_args=("--batch-mode" "--quiet" "--file=${conll_dir}" "-Dfile.encoding=UTF8")
 # Uncomment the following line for a full stack-trace
 # mvn_args=("--batch-mode" "--quiet" "-e")
 mvn_compile_goal=("compile")
-mvn_exec_goal=("exec:java" "-Dexec.mainClass=org.acoli.conll.rdf.$class_name" "-Dexec.args=${quoted_args}")
+mvn_exec_goal=("exec:java" "-Dexec.mainClass=org.acoli.conll.rdf.$class_name" "-Dexec.args=${args}")
 
 all_args=("${mvn_args[@]}" "${mvn_compile_goal[@]}" "${mvn_exec_goal[@]}")
 
