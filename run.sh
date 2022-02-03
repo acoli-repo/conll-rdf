46e9cbb898a8410bd1a1e41d2b67963ef177bd9a
leogott <61663141+leogott@users.noreply.github.com>
2022-02-03 05:31:15 +0100

    Swap run.sh and run-via-maven.sh

--- run.sh
+++ run.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
 #
-# compile & run, using maven, the specified java class (of the org.acoli.conll.rdf package) with the provided arguments.
-# This is intended to be the most failproof way to use conll-rdf
-# If you need better performance (e.g. faster startup), you should probably use java on a jar-with-dependencies instead.
+# Conll-rdf wrapper script using a jar-with-dependencies instead of the default maven-exec.
+# Please read the notice at the start of run-via-maven.sh if you're not sure what you're seeing here.
 
-# To the unfortunate HiWi or post-graduate tasked with modifying this file after I'm gone: Good luck!
-# Please read up on Quotes in bash before modifying this file. See https://mywiki.wooledge.org/Quotes for example.
-# Also http://mywiki.wooledge.org/WordSplitting while you're at it.
-
-# Configure Bash Strict Mode
-# * set -e to exit on error
-# * set -u to fail on references to unbound variables
-# * set -o pipefail to carry the non-zero exit code of a pipeline
-# * IFS=$'\n\t' (InternalFieldSeperator) to not split arrays on spaces. Notice the $'...' syntax
-# Uncomment the following two lines to enable (disabled because running this like `. run.sh`
-# would apply these settings to your console and terminate bash on error)
-# set -euo pipefail
-# IFS=$'\n\t'
-
-# store the path of the conll-rdf directory
-# $0 is a special variable containing the location of the script
+# store the path of the conll-rdf/target directory
 conll_dir="$(dirname -- "$(realpath -- "$0")")"
-# use like target_dir="${conll_dir}/target/classes"
-
-# join(char seperator, arg1 ... argn) concatenates an array with the seperating character
-function join {
-  local IFS="$1"
-  shift
-  printf '%s\n' "$*"
-}
+target_dir="${conll_dir}/target"
+package_jar="${target_dir}/conll-rdf-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
 # if the number of arguments is equal to 0, no class name is provided
 if [ $# -eq 0 ]; then
@@ -36,33 +14,13 @@ if [ $# -eq 0 ]; then
 	exit 1
 fi
 
-# first cli argument
-class_name="$1"
-# "consume" 1 argument
-shift
-# $@ is a special variable containing the arguments passed to the shell script.
-# ${variable@Q} is "Parameter Transformation" to quote the content of the array for re-use
-# as the variable is $@ is this case, ${}
-# $@ is the variable of the arguments. For a wrapper to java you'd use ("$@")
-# For an explanation to the use of quotes, see the reference at the top of this file.
-args_array=("${@@Q}")
-# join the quoted args into a single string
-args=$(join ' ' "${args_array[@]}")
+# Check for presence of packaged jar
+if [ ! -e "${package_jar}" ]; then
+    echo "Please make sure to run package.sh first, and verify the jar-with-dependencies is present in the target folder"
+    exit 1
+fi
 
-mvn_args=("--batch-mode" "--quiet" "--file=${conll_dir}" "-Dfile.encoding=UTF8")
-# Uncomment the following line for a full stack-trace
-# mvn_args=("--batch-mode" "--quiet" "-e")
-mvn_compile_goal=("compile")
-mvn_exec_goal=("exec:java" "-Dexec.mainClass=org.acoli.conll.rdf.$class_name" "-Dexec.args=${args}")
+class_name=$1
+shift 1
 
-all_args=("${mvn_args[@]}" "${mvn_compile_goal[@]}" "${mvn_exec_goal[@]}")
-
-mvn "${all_args[@]}" |
-	# the following is a hack to allow CoNLLRDFUpdater to process output of CoNLLBrackets2RDF
-	# currently, CoNLLRDFUpdater supportes the historical Turtle 1.0 prefix only
-	# TODO: fix this in CoNLLRDFUpdater, this is a hack, only
-	sed -e s/'^[\t ]*PREFIX \(.*\)$'/'@prefix \1 .'/g
-
-# The following commands were pretty useful in debugging the code above
-# echo ${#mvn_exec_goal[@]} # echo the size of the array
-# declare -p mvn_exec_goal # "describe" the array
+java -cp "${package_jar}" "org.acoli.conll.rdf.${class_name}" "$@"
